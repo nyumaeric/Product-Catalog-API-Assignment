1ecd8a08d8a147ac669fb407e194d04be29ce778
nyumaeric <133812002+nyumaeric@users.noreply.github.com>
2025-03-21 13:04:59 +0000

    assig

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Add a route to handle POST requests
+app.post('/', (req, res) => {
+  res.send('POST request received');
+});
+
+app.put('/', (req, res) => {
+  res.send('PUT request received');
+});
+
+app.delete('/', (req, res) => {
+  res.send('DELETE request received');
+});
+
+app.patch('/', (req, res) => {
+  res.send('PATCH request received');
+});
+
+app.options('/', (req, res) => {
+  res.send('OPTIONS request received');
+});
+
+app.head('/', (req, res) => {
+  res.send('HEAD request received');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
